8aff8e86cf884f02863305790007de834619fcf2
micamarietta <44626975+micamarietta@users.noreply.github.com>
2022-04-23 11:19:22 -0700

    Updated project to include ride request functionality

--- VacTrack/VacTrack/Controllers/RideRequestController.cs
+++ VacTrack/VacTrack/Controllers/RideRequestController.cs
@@ -8,9 +8,6 @@ using Microsoft.Extensions.Logging;
 using PantherPickup.Models;
 using PantherPickup.Models.Account;
 using PantherPickup.Utilities;
-using PantherPickup.Models.Patient;
-using PantherPickup.Models.Passenger;
-using PantherPickup.Models.VaccinationLocation;
 
 namespace PantherPickup.Controllers
 {
@@ -54,58 +51,6 @@ namespace PantherPickup.Controllers
             return View();
         }
 
-        public ActionResult Profile()
-        {
-            var model = new AccountModel();
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(Configuration.GetConnectionString("PantherPickup")))
-                {
-                    SqlCommand command = new SqlCommand("SELECT * FROM person WHERE email = 'marietta@chapman.edu' ", connection);
-                    command.Connection.Open();
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        model.Name = reader["name"].ConvertFromDBVal<string>();
-                        model.Email = reader["email"].ConvertFromDBVal<string>();
-                        model.IsPassenger = reader["isPassenger"].ConvertFromDBVal<bool>();
-                        model.Major = reader["major"].ConvertFromDBVal<string>();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return View("Error", new ErrorViewModel { ErrorMessage = ex.Message });
-            }
-            return View(model);
-        }
-
-        public ActionResult Calendar()
-        {
-            //probably should take in a ride request model
-            var model = new AccountModel();
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(Configuration.GetConnectionString("PantherPickup")))
-                {
-                    SqlCommand command = new SqlCommand("SELECT * FROM person WHERE email = 'marietta@chapman.edu' ", connection);
-                    command.Connection.Open();
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        model.Name = reader["name"].ConvertFromDBVal<string>();
-                        model.Email = reader["email"].ConvertFromDBVal<string>();
-                        model.IsPassenger = reader["isPassenger"].ConvertFromDBVal<bool>();
-                        model.Major = reader["major"].ConvertFromDBVal<string>();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return View("Error", new ErrorViewModel { ErrorMessage = ex.Message });
-            }
-            return View(model);
-        }
 
     }
 }
